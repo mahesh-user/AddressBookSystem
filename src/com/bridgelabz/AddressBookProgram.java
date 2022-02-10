d0f7346d3c8c19b82feb87e585b057c4e9a5db78
Mahesh <mahesh51138@gmail.com>
2022-02-10 15:48:55 +0530

    added:DeleteContactInAccountBook

--- src/com/bridgelabz/AddressBookProgram.java
+++ src/com/bridgelabz/AddressBookProgram.java
@@ -2,11 +2,11 @@ package com.bridgelabz;
 
 public class AddressBookProgram {
 
-            public static void main (String[] args){
-            ContactsManager contactsManager = new ContactsManager();
-            contactsManager.add();
-                System.out.println("cont man main------");
-            contactsManager.edit();
+    public static void main(String[] args) {
+        ContactsManager contactsManager = new ContactsManager();
+        contactsManager.add();
+        contactsManager.add();
+        contactsManager.delete();
 
-        }
+    }
     }
--- src/com/bridgelabz/ContactsManager.java
+++ src/com/bridgelabz/ContactsManager.java
@@ -4,9 +4,9 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class ContactsManager {
-    ArrayList<Contact> list = new ArrayList<>();
-    public void add(){
-        System.out.println("cont man------");
+    ArrayList<Contact> list = new ArrayList<Contact>();
+
+    public void add() {
         Scanner sc = new Scanner(System.in);
         String name = sc.next();
         String address = sc.next();
@@ -15,26 +15,33 @@ public class ContactsManager {
         String email = sc.next();
         long phoneNumber = sc.nextLong();
         long zipCode = sc.nextLong();
-        Contact contact = new Contact(name,address,city,state,email,phoneNumber,zipCode);
+        Contact contact = new Contact(name, address, city, state, email, phoneNumber, zipCode);
         list.add(contact);
-        System.out.println("add-------------");
     }
-    public void edit(){
-        Scanner scanner = new Scanner(System.in);
-        System.out.println(" edit the addressbook");
-        String name1 = scanner.next();
 
-        for (int i=0;i< list.size();i++){
-            if ((list.get(i).name).equals(name1)){
+    public void edit() {
+        Scanner scanner = new Scanner(System.in);
+        String name1 = scanner.next();
+        for (int i = 0; i < list.size(); i++) {
+            if ((list.get(i).name).equals(name1)) {
                 list.get(i).phoneNumber = 93758723;
                 list.get(i).address = "agjishg";
                 list.get(i).city = "ahfaj";
-                System.out.println("address");
-                System.out.println("phonenumber");
-                System.out.println("city");
-
             }
         }
 
+    }
+
+    public void delete() {
+        Scanner scanner = new Scanner(System.in);
+        String name1 = scanner.next();
+        for (int i = 0; i < list.size(); i++) {
+            if ((list.get(i).name).equals(name1)) {
+                list.remove(i);
+            }
+        }
+        for (Contact i:list) {
+            System.out.println(i.name);
+        }
     }
 }
